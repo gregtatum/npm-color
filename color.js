4b8ca2f7cf24b3f0efbffb54c93c86b9a1adfcf4
Greg Tatum <tatum.creative@gmail.com>
2015-10-17 22:00:29 -0500

    Required the wrong file

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 module.exports = {
 	hslToRgb    : require('./lib/hsl-to-rgb'),
-	hslToHex    : require('./lib/hsl-to-rgb'),
+	hslToHex    : require('./lib/hsl-to-hex'),
 	hexToRgb    : require('./lib/hex-to-rgb'),
 	hexToString : require('./lib/hex-to-string'),
 	rgbToHex    : require('./lib/rgb-to-hex'),
