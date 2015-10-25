bdfe8ce790c83a6f840295a7b1d5d6d18f824412
Greg Tatum <tatum.creative@gmail.com>
2015-10-25 09:25:30 -0500

    Missed hooking up some files

--- color.js
+++ color.js
@@ -1,9 +1,11 @@
 module.exports = {
 	hslToRgb    : require('./lib/hsl-to-rgb'),
 	hslToHex    : require('./lib/hsl-to-hex'),
+	hslToStyle  : require('./lib/hsl-to-style'),
 	hexToRgb    : require('./lib/hex-to-rgb'),
 	hexToString : require('./lib/hex-to-string'),
 	rgbToHex    : require('./lib/rgb-to-hex'),
-	rgbToHsl    : require('./lib/rgb-to-Hsl'),
+	rgbToHsl    : require('./lib/rgb-to-hsl'),
+	rgbToStyle  : require('./lib/rgb-to-style'),
 	stringToHex : require('./lib/string-to-hex'),
 }
